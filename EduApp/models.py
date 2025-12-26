ec5129b9a9acbd7bf42c66e4a87940b8cd0c186b
Pavel <pavelmis03@gmail.com>
2025-12-26 13:32:05 +0300

    added first models

--- EduApp/models.py
+++ EduApp/models.py
@@ -1,13 +1,13 @@
-'''from django.db import models
+from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length = 30)
+    name = models.CharField(max_length=30)
     age = models.IntegerField()
-    surname = models.CharField(max_length = 30)
+    surname = models.CharField(max_length=30)
     email = models.EmailField()
-    password = models.CharField(max_length = 30)
-    class_id = models.CharField(max_length = 2)
-    about = models.CharField(max_length = 300)
+    password = models.CharField(max_length=30)
+    class_id = models.CharField(max_length=2)
+    about = models.CharField(max_length=300)
     birth_date= models.DateField()
     course_cnt = models.IntegerField()
     hours = models.IntegerField()
@@ -18,16 +18,17 @@ class User(models.Model):
 
 
 class Task(models.Model):
-    description = models.CharField(max_length = 300)
-    title = models.CharField(max_length = 300)
+    description = models.CharField(max_length=300)
+    title = models.CharField(max_length=300)
     number = models.IntegerField()
-    number_of_hours = models.IntegerField()
-    answers = models.CharField(max_length = 300)
+    # number_of_hours = models.IntegerField()
+    answers = models.CharField(max_length=300)
+    goodAnswers = models.IntegerField()
+    badAnswers = models.IntegerField()
 
 
 
-
-
+'''
 #описание, заголовок, номер, количество решений, ответ
     def __init__(self, description, title, number, number_of_solutions, answer):
         self.description = description
