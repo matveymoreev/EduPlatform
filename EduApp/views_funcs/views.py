d4c82f13b0fe372eb09a1078cdcccd45c1e96970
Pavel <pavelmis03@gmail.com>
2026-01-18 21:22:24 +0300

    added comments

--- EduApp/views_funcs/views.py
+++ EduApp/views_funcs/views.py
@@ -13,8 +13,6 @@ from django.contrib.auth import login, logout, authenticate
 
 def main(request):
 
-
-
     return render(request, "pages_main/main_page.html")
 
 
@@ -46,8 +44,6 @@ def ege_subjects(request):
 
 
 
-
-
 def number(request):
     data = {
         "text": "На тарелке лежат одинаковые на вид пирожки: 4 с мясом, 8 с капустой и 3 с вишней. Петя наугад берёт один пирожок. Найдите вероятность того, что пирожок окажется с вишней.",
@@ -63,16 +59,24 @@ def number(request):
 def books(request):
     return render(request, "pages_main/books_page.html")
 
+# войти в аккаунт
 def login_page(request):
     reg_data = {}
+    # проверяем, что запрос был на отправку данных формы на сервер
     if request.method == "POST":
+        # получаем данные из запроса из полей формы email и password
         reg_data["email"] = request.POST.get["email"]
         reg_data["password"] = request.POST.get["password"]
+    # проверить, что есть в БД
     return render(request, "pages_main/login_page.html")
 
+# выйти из аккаунта
 def logout_page(request):
+    # ДОДЕЛАТЬ ВЫХОД
+    # logout(request)
     return render(request, "pages_main/logout_page.html")
 
+# авторизоваться
 def auth_page(request):
     reg_data = {}
     context = {}
@@ -86,96 +90,33 @@ def auth_page(request):
         reg_data["check_terms"] = request.POST.get("check_terms", "undefined")
         reg_data["user_class"] = request.POST.get("user_class", "undefined")
 
+        # обращаемся к таблице User в БД, получаем через objects все записи в таблице
+        # фильтруем данные по параметру email
         if (len(User.objects.filter(email=reg_data["email"])) or len(DefUser.objects.filter(email=reg_data["email"]))):
             context["errors"] = "Логин: Пользователь с таким логином уже существует!"
+            # возвращаем страницу с выводом ошибки
             return render(request, "pages_main/auth_page.html", context=context)
+        # проверка возраста
         if (reg_data["age"] > 100):
             context["errors"] = "Дата рождения: Ошибка даты рождения!"
             return render(request, "pages_main/auth_page.html", context=context)
+        # проверяем совпадение полей пароль и повторить пароль
         if (reg_data["password"] != reg_data["password_repeat"]):
             context["errors"] = "Пароль: Ошибка при повторении пороля!"
             return render(request, "pages_main/auth_page.html", context=context)
 
+        # если ошибок в заполнении формы не было, идем дальше
 
         # добавляем пользователя в нашу таблицу в БД
         user = User.objects.create(name=reg_data["name"], age=reg_data["age"], surname=reg_data["surname"], email=reg_data["email"], password=reg_data["password"],
                                    class_id=0, about="",  birth_date="2003-12-12", course_cnt=0, hours=0, progress=0, activity=0)
         # добавляем пользователя в стандартную таблицу зарегистрированных пользователей
         usr = DefUser.objects.create_user(reg_data["email"], reg_data["email"], reg_data["password"], first_name=reg_data["name"], last_name=reg_data["surname"])
+        # системная функция авторизации
         login(request, usr)
         # если успешно вошли, меняем кнопки регистрации и войти на кнопку аккаунта
         context["logIn"] = True
+        # если авторизация прошла успешно, переходим на главную страницу
         return redirect("/")
 
     return render(request, "pages_main/auth_page.html", context=context)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
